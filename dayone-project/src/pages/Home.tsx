48d5ea2857d195d5c99e056ce5b36310c0d87a74
Leeseayoung <tnfusrltk12@naver.com>
2024-09-04 22:06:02 +0900

    스타일 컨포넌트 인식이 안됨 그래서 일단 때려 밖음

--- dayone-project/src/pages/Home.tsx
+++ dayone-project/src/pages/Home.tsx
@@ -1,25 +1,102 @@
-import { useEffect } from "react";
+import styled from "styled-components";
 import { Link } from "react-router-dom";
 
-function Home() {
-  useEffect(() => {
-    const featchData = fetch("https://jsonplaceholder.typicode.com/todos/1")
-      .then((response) => response.json())
-      .then((json) => console.log(json));
-    console.log(featchData);
-  }, []);
-
+const Home = () => {
   return (
-    <>
-      <p className="text-3xl font-bold underline">안녕하세요</p>
-      <Link to="/signIn">
-        <button>로그인</button>
-      </Link>
-      <Link to="/signUp">
-        <button>회원가입</button>
-      </Link>
-    </>
+    <Wrapper>
+      <Title>로그인하기</Title>
+      <Form>
+        <Inputs>
+          <Input />
+          <Input />
+        </Inputs>
+        <Button>Login</Button>
+      </Form>
+      <CustomLink to="/signup">회원가입하기</CustomLink>
+    </Wrapper>
   );
-}
+};
 
 export default Home;
+
+const Button = styled.button`
+  background-color: black;
+  color: white;
+  padding: 20px;
+  border-radius: 10px;
+`;
+const CustomLink = styled(Link)`
+  margin-top: 20px;
+  color: black;
+  text-decoration: none;
+  &:visited {
+    color: black;
+    text-decoration: none;
+  }
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-items: center;
+  flex-direction: column;
+`;
+
+const Title = styled.div`
+  font-size: 30px;
+  font-weight: 700;
+  margin-bottom: 30px;
+`;
+
+const Input = styled.input`
+  font-size: 20px;
+  height: 30px;
+  border-radius: 10px;
+  border: none;
+  padding: 10px;
+  &::placeholder {
+    color: darkgray;
+    font-size: 20px;
+    font-weight: 500;
+    font-family: "OTWelcomeRA";
+  }
+`;
+
+const Inputs = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  gap: 10px;
+  margin-right: 10px;
+`;
+
+const Form = styled.div`
+  display: flex;
+  height: 100%;
+`;
+
+// import { useEffect } from "react";
+// import { Link } from "react-router-dom";
+
+// function Home() {
+//   useEffect(() => {
+//     const featchData = fetch("https://jsonplaceholder.typicode.com/todos/1")
+//       .then((response) => response.json())
+//       .then((json) => console.log(json));
+//     console.log(featchData);
+//   }, []);
+
+//   return (
+//     <>
+//       <p className="text-3xl font-bold underline">안녕하세요</p>
+//       <Link to="/signIn">
+//         <button>로그인</button>
+//       </Link>
+//       <Link to="/signUp">
+//         <button>회원가입</button>
+//       </Link>
+//     </>
+//   );
+// }
+
+// export default Home;
